ad5f29208693377d49ffaa9e03024c5dd6a93d29
lolsuperscratch <lolsuperscratch@hotmail.com>
2018-10-23 01:34:06 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ var client = new Discord.Client({disableEveryone:true})
 var channelid = "504118969674760193";
 client.on('ready', function () {
   client.setInterval(function (){
-      client.user.setActivity(`with ${client.guilds.length} guilds in emote pack`, { type: 'PLAYING' })
+      client.user.setActivity(`with emote packs`, { type: 'PLAYING' })
   },6000)
 });
 client.on('message',function (message) {
@@ -11,8 +11,9 @@ client.on('message',function (message) {
     if (message.content.split(' ')[0] == "!invite") {
        if (!message.content.split(' ')[1]) return message.channel.send(':warning: empty. but cannot be added on our server.')
       var emotepack = client.channels.get(channelid)
-       emotepack.send(`**${guild.name}**`)
-       emotepack.send(`${guild.emojis.map(e => String(e)).join('')} ${message.content.split(' ')[1]}`)
+       emotepack.send(`**${message.guild.name}**`)
+       emotepack.send(`${message.guild.emojis.map(e => String(e)).join('')} ${message.content.split(' ')[1]}`)
+       message.channel.send('good, your emote pack has been added. enjoy!')
     }
 })
 
