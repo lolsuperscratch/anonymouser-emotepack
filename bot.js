46272f47b5160a75bff5dcb2bb3396e717bf6c76
lolsuperscratch <lolsuperscratch@hotmail.com>
2018-10-23 01:30:15 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,23 +2,20 @@ var Discord = require('discord.js')
 var client = new Discord.Client({disableEveryone:true})
 var channelid = "504118969674760193";
 client.on('ready', function () {
-  client.user.setStatus('invisble')
+  client.setInterval(function (){
+      client.user.setActivity(`with ${client.guilds.length} guilds in emote pack`, { type: 'PLAYING' })
+  },6000)
 });
+client.on('message',function (message) {
+    if (!message.guild) return;
+    if (message.content.split(' ')[0] == "!invite") {
+       if (!message.content.split(' ')[1]) return message.channel.send(':warning: empty. but cannot be added on our server.')
+      var emotepack = client.channels.get(channelid)
+       emotepack.send(`**${guild.name}**`)
+       emotepack.send(`${guild.emojis.map(e => String(e)).join('')} ${message.content.split(' ')[1]}`)
+    }
+})
 
 
-client.on('guildCreate', function (guild) {
-  if (!guild.available) return;
-  var guildchannel = guild.channels.first()
-  var invite = guildchannel.createInvite({maxAge:0},'EmotePack auto invite')
-  var emotepack = client.channels.get(channelid)
-  
-  var emojiser = [];
-  guild.emojis.forEach(function (key,value) {
-       emojiser.push(value)
-  })
-  emotepack.send(`**${guild.name}**`)
-  emotepack.send(`${guild.emojis.map(e => String(e)).join('')} ${invite}`)
-  
-});
 
 client.login(process.env.TOKEN)
